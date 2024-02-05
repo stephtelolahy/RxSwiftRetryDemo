d1f8a96af36a06b1e77307201d8d3ff878456640
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-02-05 06:22:32 +0100

    Initial Commit

---
+++ RxSwiftRetryDemo/RxSwiftRetryDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwiftRetryDemoApp.swift
+//  RxSwiftRetryDemo
+//
+//  Created by Hugues Telolahy on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RxSwiftRetryDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
